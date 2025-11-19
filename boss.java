07f253b15dd473c729135c64fc2ed052604e171c
piya-co <piyawat.jar@student.mahidol.edu>
2025-11-20 03:30:45 +0700

    Update boss.java

--- boss.java
+++ boss.java
@@ -1,67 +1,88 @@
-package Project3_xxx;
+package Project3_6713249;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.*;
 
 public abstract class boss extends JLabel {
 
+    // reference to main game engine
     protected GameEngine game;
+
+    // difficulty: 0 = easy, 1 = medium, 2 = hard
     protected int difficulty;
 
+    // stats
     protected int maxHP;
     protected int hp;
-    protected int moveSpeed;
-    protected int attackInterval;
-    protected int contactDamage;
+    protected int moveSpeed;       // pixels per movement step
+    protected int attackInterval;  // milliseconds between attacks
+    protected int contactDamage;   // damage when boss attack hits player
 
+    // images / sprites
     protected MyImageIcon imgNormal;
     protected MyImageIcon imgAttack;
     protected MyImageIcon imgHurt;
     protected MyImageIcon imgDead;
 
+    // timers (created in subclasses)
     protected Timer moveTimer;
     protected Timer attackTimer;
 
+    // ---------- constructor ----------
     public boss(GameEngine game, int difficulty) {
         this.game = game;
         this.difficulty = difficulty;
 
-        setOpaque(false);
+        setOpaque(false);        // we will use transparent PNGs
+        setSize(200, 150);       // default size; boss1/2/3 can change with setBounds()
     }
 
+    // ---------- control timers ----------
+    // called by GameEngine after boss is created
     public void start() {
-        if (moveTimer != null) moveTimer.start();
+        if (moveTimer   != null) moveTimer.start();
         if (attackTimer != null) attackTimer.start();
     }
 
     public void stop() {
-        if (moveTimer != null) moveTimer.stop();
+        if (moveTimer   != null) moveTimer.stop();
         if (attackTimer != null) attackTimer.stop();
     }
 
+    // ---------- sprite helper ----------
+    // subclasses call this once after loading resized images
     protected void setSprites(MyImageIcon normal,
                               MyImageIcon attack,
                               MyImageIcon hurt,
                               MyImageIcon dead) {
-
         this.imgNormal = normal;
         this.imgAttack = attack;
         this.imgHurt   = hurt;
         this.imgDead   = dead;
 
-        if (imgNormal != null) setIcon(imgNormal);
+        if (imgNormal != null) {
+            setIcon(imgNormal);
+        }
     }
 
+    // ---------- HP / damage ----------
     public void takeDamage(int dmg) {
         hp -= dmg;
         if (hp < 0) hp = 0;
 
+        // tell GameEngine so it can update the boss HP bar
+        if (game != null) {
+            game.damageBoss(dmg);    // make sure you implement this in GameEngine
+        }
+
         if (hp == 0) {
-            if (imgDead != null) setIcon(imgDead);
+            if (imgDead != null) {
+                setIcon(imgDead);
+            }
             stop();
+            // later: game.gameOver(true); // player wins
         } else {
-            if (imgHurt != null) {
+            if (imgHurt != null && imgNormal != null) {
                 setIcon(imgHurt);
                 new Timer(150, e -> {
                     setIcon(imgNormal);
@@ -71,14 +92,10 @@ public abstract class boss extends JLabel {
         }
     }
 
-    protected boolean hitPlayer(Rectangle box) {
-        PlayerLabel p = game.getPlayerLabel();
-        if (p == null) return false;
-        return box.intersects(p.getBounds());
-    }
-
-    protected abstract void initStats();
-
-    public int getHP() { return hp; }
+    public int getHP()    { return hp; }
     public int getMaxHP() { return maxHP; }
+
+    // each concrete boss (boss1, boss2, boss3) must implement this
+    // and set maxHP, moveSpeed, attackInterval, contactDamage depending on difficulty
+    protected abstract void initStats();
 }
