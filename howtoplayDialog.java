dd9a047b9998459a38748a9190362fc26386a54c
piya-co <piyawat.jar@student.mahidol.edu>
2025-11-26 12:08:26 +0700

    Update howtoplayDialog.java

--- howtoplayDialog.java
+++ howtoplayDialog.java
@@ -1,4 +1,3 @@
-
 package Project3_6713249;
 
 import java.awt.event.MouseAdapter;
@@ -11,7 +10,7 @@ import java.util.*;
 import javax.swing.JLabel;
 
 public class howtoplayDialog extends SelectionDialog {
-    
+
     public howtoplayDialog(String bg_path, String name, mainFrame owner) {
         super(bg_path, name, owner);
         returnButton = new menuButtonLabel(constants.OKBUTTON, constants.OKBUTTON_HOVER, 200, 75, owner);
@@ -20,27 +19,10 @@ public class howtoplayDialog extends SelectionDialog {
             @Override
             public void mouseClicked(MouseEvent e) {
                 if (SwingUtilities.isRightMouseButton(e) || e.getButton() == MouseEvent.BUTTON2) {
-                        JLabel msg2 = new JLabel("Can't click");
-                        msg2.setFont(new Font("Monospaced", Font.BOLD, 20));
-                        msg2.setForeground(Color.RED);
-                        msg2.setBounds(constants.frameWidth - 200 - margin, constants.frameHeight - 190 - margin, frameWidth, 180);
-                        contentpane.add(msg2);
-                        contentpane.revalidate();
-                        contentpane.repaint();
-                        
-                        new javax.swing.Timer(2000, ev -> {
-                        contentpane.remove(msg2);
-                        contentpane.revalidate();
-                        contentpane.repaint();
-                    }) {
-                        {
-                            setRepeats(false);   // ให้ทำครั้งเดียว
-                        }
-                    }.start();
 
-                        System.out.println("click ignored");
-                        return; // 
-                    }
+                    System.out.println("click ignored");
+                    return; // 
+                }
                 System.out.println("Clicked: ok");
                 owner.setVisible(true);
                 dispose();
@@ -62,6 +44,6 @@ public class howtoplayDialog extends SelectionDialog {
         });
         contentpane.add(returnButton);
         setVisible(true);
-        
-}
+
+    }
 }
